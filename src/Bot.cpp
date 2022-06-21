6a5da59d7529b792abdb2cf821a26833480dc5b1
mwlodarzc <mwlodarzc@gmail.com>
2022-06-21 13:59:39 +0200

    Ready

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -25,7 +25,6 @@ int Bot::minimax(CheckerBoard &position, int depth, int alfa, int beta, bool whi
         minScore = std::numeric_limits<int>::max();
         for (uint i = 0; i < possible_positions.size(); i++)
         {
-            // std::cout << possible_positions[i] << std::endl;
             score = minimax(possible_positions[i], depth - 1, alfa, beta, true);
             minScore = std::min(minScore, score);
             beta = std::min(beta, score);
@@ -41,13 +40,15 @@ void Bot::make_best_move(CheckerBoard &position, int depth, bool whitePov)
 {
     int maxScore, score, index = 0;
     std::vector<CheckerBoard> possible_positions = position.get_all_settings();
-    maxScore = minimax(position, depth, -std::numeric_limits<int>::max(), std::numeric_limits<int>::max(), whitePov);
-
+    maxScore = -std::numeric_limits<int>::max();
+    if (whitePov)
+        whitePov = !whitePov;
     if (!possible_positions.empty())
         for (uint i = 0; i < possible_positions.size(); i++)
         {
             score = minimax(possible_positions[i], depth - 1, -std::numeric_limits<int>::max(), std::numeric_limits<int>::max(), whitePov);
-            if (score == maxScore)
+            std::cout << score << std::endl;
+            if (score < maxScore)
             {
                 maxScore = score;
                 index = i;
